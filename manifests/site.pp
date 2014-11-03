d97f71294a323cb1b331a4c8301cca8bf3364197
Kent Holloway <drizit@gmail.com>
2014-11-02 22:35:00 -0600

    Adding python, pyenv and most of the modules I use

--- manifests/site.pp
+++ manifests/site.pp
@@ -76,6 +76,16 @@ node default {
   #ruby::version { '2.1.0': }
   #ruby::version { '2.1.1': }
 
+  # Get the Xcode path to include files
+  $include_path = inline_template('<%= %x{xcrun --show-sdk-path}.strip %>')
+
+  # Link the Xcode include path to /usr/include to fix a lot of errors from happening
+  # during ruby and python modules build time
+  file { '/usr/include':
+    ensure => link,
+    target => "${include_path}/usr/include",
+  }
+
   # Set our new Ruby version
   # NOTE: You can also specify versions to install as shown a few lines above
   # See the Puppet-ruby module on Github for more info:
@@ -94,7 +104,16 @@ node default {
     version      => '~> 1.0',
     ruby_version => '*',
   }
-
+  ruby_gem { "puppet for ${ruby_version}":
+    gem          => 'puppet',
+    version      => '~> 3.4.0',
+    ruby_version => $ruby_version,
+  }
+  ruby_gem { "facter for ${ruby_version}":
+    gem          => 'facter',
+    version      => '~> 1.7.6',
+    ruby_version => $ruby_version,
+  }
   ruby_gem { "puppet-lint for ${ruby_version}":
     gem          => 'puppet-lint',
     version      => '~> 1.0.1',
@@ -144,6 +163,125 @@ node default {
     ],
   }
 
+  # Oddball setup for Python on 10.9 and 10.10
+  $python_version = '2.7.8'
+  exec { "python ${python_version}":
+    command => "env -i bash -c 'source /opt/boxen/env.sh && CFLAGS=\"-I$(xcrun --show-sdk-path)/usr/include\" pyenv install ${python_version}'",
+    creates => "/opt/boxen/pyenv/versions/${python_version}/bin/python",
+  }
+
+  # Normally this does the actual install but due to a bug the exec above is required
+  python::version { $python_version: }
+
+  # Set pyenv_version in Hiera /opt/boxen/repo/hiera/common.yaml like the line below
+  #  python::pyenv_version: "v20141012"
+
+  # This sets the version that pyenv uses everywhere to our version
+  class { 'python::global':
+    version => $python_version,
+  }
+
+  python::package { "fabric for ${python_version}":
+    package => 'fabric',
+    python  => $python_version,
+    version => '>=1.9.1',
+  }
+  python::package { "markdown for ${python_version}":
+    package => 'markdown',
+    python  => $python_version,
+    version => '>=2.4.1',
+  }
+  python::package { "mako for ${python_version}":
+    package => 'mako',
+    python  => $python_version,
+    version => '>=1.0.0',
+  }
+  python::package { "bottle for ${python_version}":
+    package => 'bottle',
+    python  => $python_version,
+    version => '>=0.12.7',
+  }
+  python::package { "SQLAlchemy for ${python_version}":
+    package => 'sqlalchemy',
+    python  => $python_version,
+    version => '>=0.9.7',
+  }
+  python::package { "ansible for ${python_version}":
+    package => 'ansible',
+    python  => $python_version,
+    version => '>=1.7.2',
+  }
+  python::package { "pssh for ${python_version}":
+    package => 'pssh',
+    python  => $python_version,
+    version => '>=2.3.1',
+  }
+  python::package { "requests for ${python_version}":
+    package => 'requests',
+    python  => $python_version,
+    version => '>=1.0.4',
+  }
+  python::package { "suds for ${python_version}":
+    package => 'suds',
+    python  => $python_version,
+    version => '>=0.4',
+  }
+  python::package { "sqlsoup for ${python_version}":
+    package => 'sqlsoup',
+    python  => $python_version,
+    version => '>=0.9.0',
+  }
+  python::package { "pyzmq for ${python_version}":
+    package => 'pyzmq',
+    python  => $python_version,
+    version => '>=14.3.1',
+  }
+  python::package { "psycopg2 for ${python_version}":
+    package => 'psycopg2',
+    python  => $python_version,
+    version => '>=2.5.3',
+  }
+  python::package { "Sphinx for ${python_version}":
+    package => 'sphinx',
+    python  => $python_version,
+    version => '>=1.2.2',
+  }
+  python::package { "awscli for ${python_version}":
+    package => 'awscli',
+    python  => $python_version,
+    version => '>=1.4.2',
+  }
+  python::package { "dnspython for ${python_version}":
+    package => 'dnspython',
+    python  => $python_version,
+    version => '>=1.11.1',
+  }
+  python::package { "luigi for ${python_version}":
+    package => 'luigi',
+    python  => $python_version,
+    version => '>=1.0.16',
+  }
+  python::package { "pytz for ${python_version}":
+    package => 'pytz',
+    python  => $python_version,
+    version => '>=2014.4',
+  }
+  python::package { "python-ldap for ${python_version}":
+    package => 'python-ldap',
+    python  => $python_version,
+    version => '>=2.4.18',
+  }
+
+  # cx-Oracle requires a bunch of crap first, this will break for anyone else
+  # Need these values in the shell
+  #   LD_LIBRARY_PATH=:/Users/kholloway/lib/oracle/instantclient_11_2
+  #   ORACLE_HOME=/Users/kholloway/lib/oracle/instantclient_11_2
+  python::package { "cx-Oracle for ${python_version}":
+    package => 'cx-oracle',
+    python  => $python_version,
+    version => '>=5.1.3',
+  }
+
   # common, useful packages via Homebrew
   $brew_packages = [
       'ack',
@@ -152,6 +290,12 @@ node default {
       'coreutils',
       'libxml2',
       'keychain',
+      'boot2docker',
+      'postgresql',
+      'gsasl',
+      'curl',
+      'iftop',
+      'tree',
       'go',
       'htop-osx',
       'jq',
@@ -159,6 +303,7 @@ node default {
       'nmap',
       'mercurial',
       'lynx',
+      'zsh',
       'gnu-tar'
   ]
 
@@ -168,6 +313,8 @@ node default {
   $brewcask_packages = [
     'iterm2',
     'mou',
+    'qlmarkdown',
+    'vagrant',
     'chicken'
   ]
 
